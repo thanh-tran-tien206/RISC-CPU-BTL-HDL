a9ceae1d802f2ceadb246a0a394a1ad5d923d930
thanh-tran-tien206 <thanh07122006@gmail.com>
2026-05-16 18:50:34 +0700

    Add files via upload

--- controller.v
+++ controller.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/09/2026 02:20:02 PM
-// Design Name: 
-// Module Name: controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module controller (
     input clk,
